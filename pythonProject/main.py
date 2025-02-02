4ba869779528bb741a66d69c034a13aadaeba40e
Antoine Ho <34996565+AntoineHo6@users.noreply.github.com>
2025-02-02 17:33:28 -0500

    can compress string

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,7 +1,51 @@
-from queue import PriorityQueue
-
 from huffmanNode import HuffmanNode
 import heapq
+from bitarray import bitarray
+import sys
+
+
+def buildHuffmanTree(freqTable: dict[str, int]) -> HuffmanNode:
+    # min heap
+    priorityQ = []
+
+    for char, freq in freqTable.items():
+        heapq.heappush(priorityQ, HuffmanNode(freq, char))
+
+    while len(priorityQ) > 1:
+        left = heapq.heappop(priorityQ)
+        right = heapq.heappop(priorityQ)
+
+        merged = HuffmanNode(left.freq + right.freq, char=None, left=left, right=right)
+
+        heapq.heappush(priorityQ, merged)
+
+    return priorityQ[0]
+
+
+def buildHuffmanCodes(node: HuffmanNode, currentCode="", huffmanCodes={}):
+    if node is None:
+        return
+
+    # is leaf node
+    if node.char is not None:
+        huffmanCodes[node.char] = currentCode
+        return
+
+    buildHuffmanCodes(node.left, currentCode + "0", huffmanCodes)
+    buildHuffmanCodes(node.right, currentCode + "1", huffmanCodes)
+
+    return huffmanCodes
+
+
+def convertInputToHuffmanCodes(input: str, codes: dict[str, int]) -> bytes:
+    encodedInput: list[str] = []
+    for char in input:
+        encodedInput.append(str(codes.get(char)))
+
+    bitArr = bitarray("".join(encodedInput))
+
+    return bitArr.tobytes()
+
 
 if __name__ == '__main__':
     text = input("Enter some text to compress: ")
@@ -14,21 +58,11 @@ if __name__ == '__main__':
         else:
             freqTable[char] = 1
 
+    huffmanTree: HuffmanNode = buildHuffmanTree(freqTable)
 
-def buildHuffmanTree(freqTable: dict[str, int]) -> HuffmanNode:
-    priorityQ = []
+    codes = buildHuffmanCodes(huffmanTree)
 
-    for char, freq in freqTable.items():
-        heapq.heappush(priorityQ, HuffmanNode(freq, char))
+    compressedResult = convertInputToHuffmanCodes(text, codes)
 
-    while len(priorityQ) > 1:
-        left = heapq.heappop(priorityQ)
-        right = heapq.heappop(priorityQ)
-
-        merged = HuffmanNode(left.freq + right.freq)
-
-        heapq.heappush(priorityQ, merged)
-
-    return priorityQ[0]
-
-
+    print("Size in bytes BEFORE compression: {}".format(sys.getsizeof(text)))
+    print("Size in bytes AFTER compression: {}".format(sys.getsizeof(compressedResult)))
